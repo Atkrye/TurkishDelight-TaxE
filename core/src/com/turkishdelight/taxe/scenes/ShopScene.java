5aafde69965314bf2add71e41abd40eb537ed2b6
JASApplications <Typhoon@Jamess-MacBook-Air-2.local>
2014-12-17 16:13:46 +0000

    All UI Elements now added
    - Final labels now added
    - Labels adjust position depending on positions of UI elements next to
    them

--- core/src/com/turkishdelight/taxe/scenes/ShopScene.java
+++ core/src/com/turkishdelight/taxe/scenes/ShopScene.java
@@ -3,6 +3,7 @@ package com.turkishdelight.taxe.scenes;
 import com.turkishdelight.taxe.Scene;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont.TextBounds;
 import com.turkishdelight.taxe.Game;
 import com.turkishdelight.taxe.SpriteComponent;
 import com.turkishdelight.taxe.guiobjects.Button;
@@ -16,11 +17,17 @@ public class ShopScene extends Scene {
 	SpriteComponent player1Fuel;
 	SpriteComponent activePlayer1Indicator;
 	Label player1NameLabel;
+	Label player1ScoreLabel;
+	Label player1MoneyLabel;
+	Label player1FuelLabel;
 	
 	SpriteComponent player2Money;
 	SpriteComponent player2Fuel;
 	SpriteComponent activePlayer2Indicator;
 	Label player2NameLabel;
+	Label player2ScoreLabel;
+	Label player2MoneyLabel;
+	Label player2FuelLabel;
 	
 	public void onCreate()
 	{
@@ -33,6 +40,32 @@ public class ShopScene extends Scene {
 		Add(shop);
 		// ---------------------
 		
+		// Create player 1 name label
+		Texture player1LabelText = new Texture("Clear_Button.png");
+		player1NameLabel = new Label(this, player1LabelText, Label.genericFont(Color.BLUE, 40));
+		player1NameLabel.setText("PLAYER 1");
+		player1NameLabel.setPosition(36, 730);
+		player1NameLabel.setAlignment(0);
+		Add(player1NameLabel);
+		// ---------------------
+		
+		// Create player 1 active indicator icon
+		Texture player1ActiveText = new Texture("Active_Player1_Indicator.png");
+		activePlayer1Indicator = new SpriteComponent(this, player1ActiveText, Game.shopZ);
+		activePlayer1Indicator.setPosition(17, 707);
+		activePlayer1Indicator.setSize(10, 20);
+		activePlayer1Indicator.setAlpha(1);
+		Add(activePlayer1Indicator);
+		// ---------------------
+		
+		// Create player 1 score label
+		player1ScoreLabel = new Label(this, player1LabelText, Label.genericFont(Color.BLUE, 20));
+		player1ScoreLabel.setText("Score: 12345");
+		player1ScoreLabel.setPosition(36, 683);
+		player1ScoreLabel.setAlignment(0);
+		Add(player1ScoreLabel);
+		// ---------------------
+		
 		// Create player 1 money icon
 		Texture money1Text = new Texture("Money.png");
 		player1Money = new SpriteComponent(this, money1Text, Game.shopZ);
@@ -42,70 +75,97 @@ public class ShopScene extends Scene {
 		Add(player1Money);
 		// ---------------------
 		
+		// Create player 1 money label
+		player1MoneyLabel = new Label(this, player1LabelText, Label.genericFont(Color.MAROON, 20));
+		player1MoneyLabel.setText("35353");
+		player1MoneyLabel.setPosition(player1Money.getX() + player1Money.getWidth() + 8, 648);
+		player1MoneyLabel.setAlignment(0);
+		Add(player1MoneyLabel);
+		// ---------------------
+		
 		// Create player 1 fuel icon
 		Texture fuel1Text = new Texture("Fuel.png");
 		player1Fuel = new SpriteComponent(this, fuel1Text, Game.shopZ);
-		player1Fuel.setPosition(184, 630);
+		TextBounds player1MoneyLabelBounds = player1MoneyLabel.getFont().getBounds(player1MoneyLabel.getText());
+		player1Fuel.setPosition(player1MoneyLabel.getX() + player1MoneyLabelBounds.width + 30, 630);
 		player1Fuel.setSize(20, 25);
 		player1Fuel.setAlpha(1);
 		Add(player1Fuel);
 		// ---------------------
 		
-		// Create player 1 active indicator icon
-		Texture player1ActiveText = new Texture("Active_Player1_Indicator.png");
-		activePlayer1Indicator = new SpriteComponent(this, player1ActiveText, Game.shopZ);
-		activePlayer1Indicator.setPosition(17, 692);
-		activePlayer1Indicator.setSize(10, 20);
-		activePlayer1Indicator.setAlpha(1);
-		Add(activePlayer1Indicator);
+		// Create player 1 fuel label
+		player1FuelLabel = new Label(this, player1LabelText, Label.genericFont(Color.TEAL, 20));
+		player1FuelLabel.setText("67543");
+		player1FuelLabel.setPosition(player1Fuel.getX() + player1Fuel.getWidth() + 8, 648);
+		player1FuelLabel.setAlignment(0);
+		Add(player1FuelLabel);
 		// ---------------------
 		
-		// Create player 1 name label
-		Texture player1LabelText = new Texture("Clear_Button.png");
-		player1NameLabel = new Label(this, player1LabelText, Label.genericFont(Color.BLUE, 40));
-		player1NameLabel.setText("PLAYER 1");
-		player1NameLabel.setPosition(36, 715);
-		player1NameLabel.setAlignment(0);
-		player1NameLabel.setAlpha(1);
-		Add(player1NameLabel);
-		// ---------------------
-		
-		// Create player 2 money icon
-		Texture money2Text = new Texture("Money.png");
-		player2Money = new SpriteComponent(this, money2Text, Game.shopZ);
-		player2Money.setPosition(734, 628);
-		player2Money.setSize(37, 27);
-		player2Money.setAlpha((float) 0.4);
-		Add(player2Money);
-		// ---------------------
-		
-		// Create player 2 fuel icon
-		Texture fuel2Text = new Texture("Fuel.png");
-		player2Fuel = new SpriteComponent(this, fuel2Text, Game.shopZ);
-		player2Fuel.setPosition(884, 630);
-		player2Fuel.setSize(20, 25);
-		player2Fuel.setAlpha((float) 0.4);
-		Add(player2Fuel);
-		// ---------------------
-		
-		// Create player 2 active indicator icon
-		Texture player2ActiveText = new Texture("Active_Player2_Indicator.png");
-		activePlayer2Indicator = new SpriteComponent(this, player2ActiveText, Game.shopZ);
-		activePlayer2Indicator.setPosition(995, 692);
-		activePlayer2Indicator.setSize(10, 20);
-		activePlayer2Indicator.setAlpha((float) 0.4);
-		Add(activePlayer2Indicator);
-		// ---------------------
 		
 		// Create player 2 name label
 		Texture player2LabelText = new Texture("Clear_Button.png");
 		player2NameLabel = new Label(this, player2LabelText, Label.genericFont(Color.LIGHT_GRAY, 40));
 		player2NameLabel.setText("PLAYER 2");
-		player2NameLabel.setPosition(985, 715);
+		player2NameLabel.setPosition(985, 730);
 		player2NameLabel.setAlignment(2);
 		player2NameLabel.setAlpha((float) 0.4);
 		Add(player2NameLabel);
 		// ---------------------
+		
+		// Create player 2 active indicator icon
+		Texture player2ActiveText = new Texture("Active_Player2_Indicator.png");
+		activePlayer2Indicator = new SpriteComponent(this, player2ActiveText, Game.shopZ);
+		activePlayer2Indicator.setPosition(995, 707);
+		activePlayer2Indicator.setSize(10, 20);
+		activePlayer2Indicator.setAlpha((float) 0.4);
+		Add(activePlayer2Indicator);
+		// ---------------------
+		
+		// Create player 2 score label
+		player2ScoreLabel = new Label(this, player2LabelText, Label.genericFont(Color.LIGHT_GRAY, 20));
+		player2ScoreLabel.setText("Score: 54321");
+		player2ScoreLabel.setPosition(985, 683);
+		player2ScoreLabel.setAlignment(2);
+		Add(player2ScoreLabel);
+		// ---------------------
+		
+		// Create player 2 fuel label
+		player2FuelLabel = new Label(this, player2LabelText, Label.genericFont(Color.LIGHT_GRAY, 20));
+		player2FuelLabel.setText("67543");
+		player2FuelLabel.setPosition(985, 648);
+		player2FuelLabel.setAlignment(2);
+		Add(player2FuelLabel);
+		// ---------------------
+		
+		// Create player 2 fuel icon
+		Texture fuel2Text = new Texture("Fuel.png");
+		player2Fuel = new SpriteComponent(this, fuel2Text, Game.shopZ);
+		player2Fuel.setSize(20, 25);
+		TextBounds player2FuelLabelBounds = player2FuelLabel.getFont().getBounds(player2FuelLabel.getText());
+		player2Fuel.setPosition(player2FuelLabel.getX() - player2FuelLabelBounds.width - player2Fuel.getWidth() - 8, 630);
+		player2Fuel.setAlpha((float) 0.4);
+		Add(player2Fuel);
+		// ---------------------
+		
+		// Create player 2 money label
+		player2MoneyLabel = new Label(this, player2LabelText, Label.genericFont(Color.LIGHT_GRAY, 20));
+		player2MoneyLabel.setText("98452");
+		int x = (int) player2Fuel.getX() - 30;
+		player2MoneyLabel.setPosition(x, 648);
+		player2MoneyLabel.setAlignment(2);
+		Add(player2MoneyLabel);
+		// ---------------------
+		
+		// Create player 2 money icon
+		Texture money2Text = new Texture("Money.png");
+		player2Money = new SpriteComponent(this, money2Text, Game.shopZ);
+		player2Money.setSize(37, 27);
+		TextBounds player2MoneyLabelBounds = player2MoneyLabel.getFont().getBounds(player2MoneyLabel.getText());
+		player2Money.setPosition(player2MoneyLabel.getX() - player2MoneyLabelBounds.width - player2Money.getWidth() - 8, 628);
+		player2Money.setAlpha((float) 0.4);
+		Add(player2Money);
+		// ---------------------
+		
 		
 		// Create Train button
 		Button trainButton = new Button(this) {
